4c522ba5e3b595314daedad86bb7875d9b0e6e93
Alexsandr Romanov <flying.dutchman.son99@gmail.com>
2023-11-07 14:29:41 +0700

    add HashTable class

---
+++ HashTableTask/HashTable.cs
@@ -0,0 +1,208 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace HashTableTask
+{
+    internal class HashTable<T> : ICollection<T>
+    {
+        public ICollection<T>[] listsArray;
+        private int count;
+        private int modCount;
+
+        public HashTable(int size)
+        {
+            listsArray = new ICollection<T>[size];
+        }
+
+        public HashTable(ICollection<T>[] listsArray)
+        {
+            this.listsArray = listsArray;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return listsArray.Length;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public virtual string ToString()
+        {
+            StringBuilder listsInfo = new StringBuilder();
+
+            foreach (ICollection<T> e in listsArray)
+            {
+                if (e is null)
+                {
+                    continue;
+                }
+                else
+                {
+                    listsInfo.Append(string.Join(", ", e)).Append("    ");
+                }
+            }
+
+            return listsInfo.ToString();
+        }
+
+        public void Add(T element)
+        {
+            if (element == null)
+            {
+                throw new ArgumentNullException("Переданнный аргумен не может быть null", nameof(element));
+            }
+
+            int index = Math.Abs(element.GetHashCode() % listsArray.Length);
+
+            if (listsArray[index] is null)
+            {
+                listsArray[index] = new List<T>();
+            }
+
+            listsArray[index].Add(element);
+
+            count++;
+            modCount++;
+        }
+
+        public void Add(ICollection<T> item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Переданнный аргумен не может быть null", nameof(item));
+            }
+
+            listsArray[count] = item;
+
+            count++;
+            modCount++;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(listsArray, 0, listsArray.Length);
+
+            count = 0;
+            modCount++;
+        }
+
+        public bool Contains(T item)
+        {
+            if (item == null)
+            {
+                return false;
+            }
+
+            bool isContains = false;
+
+            int index = Math.Abs(item.GetHashCode() % listsArray.Length);
+
+            if (listsArray[index] is null)
+            {
+                return false;
+            }
+
+            foreach (T e in listsArray[index])
+            {
+                if (e is not null)
+                {
+                    if (e.Equals(item))
+                    {
+                        isContains = true;
+
+                        break;
+                    }
+                }
+            }
+
+            return isContains;
+        }
+
+        public void CopyTo(T[] array, int listsArrayIndex)
+        {
+            Array.Copy(listsArray[listsArrayIndex].ToArray(), 0, array, 0, listsArray[listsArrayIndex].Count);
+        }
+
+        public bool Remove(T item)
+        {
+            if (item == null)
+            {
+                return false;
+            }
+
+            bool isDelete = false;
+
+            int index = Math.Abs(item.GetHashCode() % listsArray.Length);
+
+            if (listsArray[index] is null)
+            {
+                return false;
+            }
+
+            foreach (T e in listsArray[index])
+            {
+                if (e is not null)
+                {
+                    if (e.Equals(item))
+                    {
+                        listsArray[index].Remove(e);
+
+                        if (listsArray[index].Count == 0)
+                        {
+                            count--;
+                        }
+
+                        isDelete = true;
+                        modCount++;
+
+                        break;
+                    }
+                }
+            }
+
+            return isDelete;
+        }
+
+        public IEnumerator<ICollection<T>> GetEnumerator()
+        {
+            int currentModCount = modCount;
+
+            for (int i = 0; i < Count; ++i)
+            {
+                if (currentModCount != modCount)
+                {
+                    throw new InvalidOperationException("Недопускается изменять список.");
+                }
+
+                yield return listsArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HashTableTask/HashTableProgram.cs
@@ -0,0 +1,52 @@
+﻿namespace HashTableTask
+{
+    public class HashTableProgram
+    {
+        static void Main(string[] args)
+        {
+            HashTable<int> hashTable = new HashTable<int>(10);
+
+            List<int> numbers1 = new List<int>(3)
+            {
+                1, 2, 3
+            };
+
+            List<int> numbers2 = new List<int>(4)
+            {
+                4, 5, 6, 7
+            };
+
+            List<int> numbers3 = new List<int>(3)
+            {
+                8, 9, 10
+            };
+
+            hashTable.Add(numbers1);
+            hashTable.Add(numbers2);
+            hashTable.Add(numbers3);
+            hashTable.Add(125);
+
+            Console.WriteLine("Содержимое хэш-таблицы: " + hashTable.ToString());
+            Console.WriteLine("Наличие значения 125 в хэш-таблице: " + hashTable.Contains(125));
+
+            int[] numbers = new int[10];
+
+            hashTable.CopyTo(numbers, 1);
+
+            Console.WriteLine("Массив чисел: " + string.Join(", ", numbers));
+            Console.WriteLine("Количество элементов хэш-таблицы: " + hashTable.Count);
+
+            hashTable.Remove(125);
+
+            Console.WriteLine("Содержимое хэш-таблицы: " + hashTable.ToString());
+            Console.WriteLine("Количество элементов хэш-таблицы: " + hashTable.Count);
+
+            foreach (List<int> list in hashTable)
+            {
+                Console.WriteLine(string.Join(", ", list));
+
+                hashTable.Add(1);
+            }
+        }
+    }
+}
