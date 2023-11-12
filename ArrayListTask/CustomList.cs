630ced1f2eff4ef9b55388c66d352053085fac34
Alexsandr Romanov <flying.dutchman.son99@gmail.com>
2023-11-12 20:35:39 +0700

    corrected ArrayList class

---
+++ ArrayListTask/CustomList.cs
@@ -0,0 +1,313 @@
+﻿using System.Collections;
+
+namespace ArrayListTask
+{
+    public class CustomList<T> : IList<T>
+    {
+        private T[] items;
+        private int modCount;
+
+        public int Count { get; private set; }
+
+        public bool IsReadOnly { get => false; }
+
+        public int Capacity
+        {
+            get => items.Length;
+            set
+            {
+                if (value < Count)
+                {
+                    throw new ArgumentException($"Значение не может быть меньше количества элементов в списке. Value = {value}.", nameof(value));
+                }
+
+                if (value == items.Length)
+                {
+                    return;
+                }
+
+                if (value > 0)
+                {
+                    if (Count > 0)
+                    {
+                        Array.Resize(ref items, value);
+                    }
+                }
+                else
+                {
+                    items = Array.Empty<T>();
+                }
+            }
+        }
+
+        public CustomList()
+        {
+            items = Array.Empty<T>();
+        }
+
+        public CustomList(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(capacity), $"Размер массива не может быть меньше нуля. Capacity = {capacity}.");
+            }
+
+            if (capacity == 0)
+            {
+                items = Array.Empty<T>();
+            }
+            else
+            {
+                items = new T[capacity];
+            }
+        }
+
+        public CustomList(T[] inputItems)
+        {
+            items = inputItems;
+            Count = items.Length;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"Индекс не может быть больше, чем количество элементов в списке. Количество элементов = {Count}. Переданное значение индекса = {index}");
+                }
+
+                return items[index];
+            }
+
+            set
+            {
+                if (index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(index), $"Индекс не может быть больше, чем количество элементов в списке. Количество элементов = {Count}. Переданное значение индекса = {index}");
+                }
+
+                items[index] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "[ " + string.Join(", ", items.Where(i => i != null)) + " ]";
+        }
+
+        public void Add(T item)
+        {
+            if (Count >= items.Length)
+            {
+                IncreaseCapacity();
+            }
+
+            items[Count] = item;
+
+            Count++;
+            modCount++;
+        }
+
+        private void IncreaseCapacity()
+        {
+            if (Capacity == 0)
+            {
+                Array.Resize(ref items, 10);
+            }
+
+            Array.Resize(ref items, items.Length * 2);
+        }
+
+        public void Clear()
+        {
+            if (Count > 0)
+            {
+                Array.Clear(items, 0, Count);
+
+                Count = 0;
+                modCount++;
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            var index = Array.IndexOf(items, item);
+
+            if (index >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void CopyTo(T[] inputItems, int startIndex)
+        {
+            if (inputItems is null)
+            {
+                throw new ArgumentNullException(nameof(inputItems), "Массив не может быть null");
+            }
+
+            if (startIndex < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startIndex), $"Индекс не должен быть меньше нуля. Индекс = {startIndex}");
+            }
+
+            if (Count > inputItems.Length)
+            {
+                throw new ArgumentException("Количество элементов исходного списка больше, чем конечный индекс массива", nameof(inputItems));
+            }
+
+            Array.Copy(items, startIndex, inputItems, 0, Count - startIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            int index = Array.IndexOf(items, item);
+
+            if (index >= 0)
+            {
+                RemoveAt(index);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= Count)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Переданный индекс превышает количество элементов в списке. Количество элементов = {Count}. Переданное значение индекса = {index}");
+            }
+
+            if (index < Count - 1)
+            {
+                Array.Copy(items, index + 1, items, index, Count - index - 1);
+            }
+
+            items[Count - 1] = default;
+
+            Count--;
+            modCount++;
+        }
+
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(items, item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > Capacity)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"Некорректное значение индекса. Индекс = {index}");
+            }
+
+            if (Capacity == Count)
+            {
+                Capacity = Capacity + 1;
+            }
+
+            if (index == Capacity - 1)
+            {
+                Add(item);
+
+                return;
+            }
+
+            T outerTemp = items[index];
+
+            for (int i = index; i < Capacity; i++)
+            {
+                if (i == index)
+                {
+                    items[i] = item;
+                    continue;
+                }
+
+                T innerTemp = items[i];
+                items[i] = outerTemp;
+                outerTemp = innerTemp;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            int startModCount = modCount;
+
+            for (int i = 0; i < Count; ++i)
+            {
+                if (startModCount != modCount)
+                {
+                    throw new InvalidOperationException("Спиок изменился.");
+                }
+
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void TrimExcess()
+        {
+            int occupancyPercent = Count / Capacity * 100;
+
+            if (occupancyPercent < 90)
+            {
+                Capacity = Count;
+            }
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(ReferenceEquals(obj, this))
+            {
+                return true;
+            }
+
+            if(ReferenceEquals(obj, null) || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            CustomList<T> list = (CustomList<T>) obj;
+            
+            if(list.Capacity != Capacity)
+            {
+                return false;
+            }
+
+            if(list.Count !=  Count)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < Capacity; ++i)
+            {
+                if (!items[i].Equals(list.items[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public override int GetHashCode()
+        {
+            int prime = 37;
+            int hash = 1;
+            hash = prime * hash + Capacity;
+            hash = prime * hash + Count;
+            hash = prime * hash + (items != null ? items.GetHashCode() : 0);
+
+            return hash;
+        }
+    }
+}
--- ArrayListTask/ListProgram.cs
+++ ArrayListTask/ListProgram.cs
@@ -4,18 +4,18 @@
     {
         static void Main(string[] args)
         {
-            MyList<int> numbersList = new MyList<int>(10);
+            CustomList<int> numbersList = new CustomList<int>(10);
 
             for (int i = 0; i < numbersList.Capacity; i++)
             {
                 numbersList.Add(i);
             }
 
-            Console.WriteLine("Список чисел: " + numbersList.ToString());
+            Console.WriteLine("Список чисел: " + numbersList);
 
             numbersList.Add(10);
 
-            Console.WriteLine("Список чисел после добавления 11 элемента: " + numbersList.ToString());
+            Console.WriteLine("Список чисел после добавления 11 элемента: " + numbersList);
             Console.WriteLine("Количество элементов списка: " + numbersList.Count);
             Console.WriteLine("Вместимость списка: " + numbersList.Capacity);
             Console.WriteLine();
@@ -23,7 +23,7 @@
             bool isDelete = numbersList.Remove(10);
 
             Console.WriteLine("Результат удаления элемента со значением 10: " + isDelete);
-            Console.WriteLine("Список чисел: " + numbersList.ToString());
+            Console.WriteLine("Список чисел: " + numbersList);
             Console.WriteLine("Количество элементов списка: " + numbersList.Count);
             Console.WriteLine("Вместимость списка: " + numbersList.Capacity);
             Console.WriteLine();
@@ -31,14 +31,14 @@
             isDelete = numbersList.Remove(56);
 
             Console.WriteLine("Результат удаления элемента со значением 56: " + isDelete);
-            Console.WriteLine("Список чисел: " + numbersList.ToString());
+            Console.WriteLine("Список чисел: " + numbersList);
             Console.WriteLine("Количество элементов списка: " + numbersList.Count);
             Console.WriteLine("Вместимость списка: " + numbersList.Capacity);
             Console.WriteLine();
 
             numbersList.RemoveAt(4);
 
-            Console.WriteLine("Список чисел после удаления элемента лежащего по индексу 4: " + numbersList.ToString());
+            Console.WriteLine("Список чисел после удаления элемента лежащего по индексу 4: " + numbersList);
             Console.WriteLine("Количество элементов списка: " + numbersList.Count);
             Console.WriteLine("Вместимость списка: " + numbersList.Capacity);
             Console.WriteLine();
@@ -52,16 +52,19 @@
             Console.WriteLine("Список чисел содержит элемент со значением 17: " + isContains);
             Console.WriteLine();
 
-            MyList<int> newNumbresList = new MyList<int>(0);
-            int[] numbers = new int[5] { 2, 4, 6, 8, 10 };
-            newNumbresList.CopyTo(numbers, 0);
+            int[] numbers = new int[20];
+            numbersList.CopyTo(numbers, 0);
 
-            Console.WriteLine("Результат копирования массива чисел новым списком: " + newNumbresList.ToString());
+            Console.WriteLine("Результат копирования массива чисел новым списком: " + string.Join(", ", numbers));
             Console.WriteLine();
 
-            newNumbresList.Clear();
+            CustomList<int> newNubersList = new CustomList<int>(numbers);
 
-            Console.WriteLine("Результат очистки нового списка чисел: " + newNumbresList.ToString());
+            Console.WriteLine("Список чисел нового списка: " + newNubersList);
+
+            newNubersList.Clear();
+
+            Console.WriteLine("Результат очистки нового списка чисел: " + newNubersList);
             Console.WriteLine();
             Console.WriteLine("Ниже представлен результат работы итератора, который последовательно печатает значение элементов списка:");
             Console.WriteLine();
@@ -76,16 +79,42 @@
             numbersList.Capacity = 100;
 
             Console.WriteLine();
-            Console.WriteLine("Характеристки списка после увеличения вместимости до 100 единицы: ");
+            Console.WriteLine("Характеристики списка после увеличения вместимости до 100 единицы: " + numbersList);
             Console.WriteLine("Количество элементов списка: " + numbersList.Count);
             Console.WriteLine("Вместимость списка: " + numbersList.Capacity);
 
             numbersList.TrimExcess();
 
             Console.WriteLine();
-            Console.WriteLine("Характеристики списка после применения метода TrimExcess: ");
+            Console.WriteLine("Характеристики списка после применения метода TrimExcess: " + numbersList);
             Console.WriteLine("Количество элементов списка: " + numbersList.Count);
             Console.WriteLine("Вместимость списка: " + numbersList.Capacity);
+
+            Console.WriteLine();
+            Console.WriteLine("Индекс элемента 5 = " + numbersList.IndexOf(5));
+            Console.WriteLine("Индекс не существующего элемента 35 = " + numbersList.IndexOf(35));
+
+            numbersList.Insert(9, 10);
+
+            Console.WriteLine();
+            Console.WriteLine("Характеристики списка после применения метода Insert: " + numbersList);
+
+            int[] postalCodes1 = new int[] { 6300089, 6300090, 6300091 };
+            int[] postalCodes2 = new int[] { 6300089, 6300090, 6300091 };
+            int[] postalCodes3 = new int[] { 6300089, 6300093, 6300091 };
+
+            CustomList<int> postalOffice1 = new CustomList<int>(postalCodes1);
+            CustomList<int> postalOffice2 = new CustomList<int>(postalCodes2);
+            CustomList<int> postalOffice3 = new CustomList<int>(postalCodes3);
+
+            Console.WriteLine();
+            Console.WriteLine("Почтовое отделение 1 содержит почтовые индексы почтового отделения 2: " + postalOffice1.Equals(postalOffice2));
+            Console.WriteLine("Почтовое отделение 1 содержит почтовые индексы почтового отделения 3: " + postalOffice1.Equals(postalOffice3));
+            Console.WriteLine("Почтовое отделение 2 содержит почтовые индексы почтового отделения 3: " + postalOffice2.Equals(postalOffice3));
+            Console.WriteLine();
+            Console.WriteLine("Хэш-код первого почтового отделения: " + postalOffice1.GetHashCode());
+            Console.WriteLine("Хэш-код второго почтового отделения: " + postalOffice2.GetHashCode());
+            Console.WriteLine("Хэш-код третьего почтового отделения: " + postalOffice3.GetHashCode());
         }
     }
 }
