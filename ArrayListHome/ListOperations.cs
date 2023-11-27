3ef19d074ed4e38fa4f7ce5a56248ecb05daa342
Alexsandr Romanov <flying.dutchman.son99@gmail.com>
2023-11-27 13:50:08 +0700

    add ArrayList homework

---
+++ ArrayListHome/ListOperations.cs
@@ -0,0 +1,45 @@
+﻿namespace ArrayListHome
+{
+    internal class ListOperations
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new();
+
+            using (StreamReader reader = new("..\\..\\..\\Numbers.txt"))
+            {
+                string currentLine;
+
+                while ((currentLine = reader.ReadLine()) != null)
+                {
+                    numbers.Add(Convert.ToInt32(currentLine));
+                }
+            };
+
+            Console.WriteLine("Список после чтения файла: " + string.Join(", ", numbers.ToArray()));
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    numbers.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            Console.WriteLine("Список после удаления нечетных чисел: " + string.Join(", ", numbers.ToArray()));
+
+            List<int> uniqNumbers = new();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers.IndexOf(numbers[i], i + 1) == -1)
+                {
+                    uniqNumbers.Add(numbers[i]);
+                }
+            }
+
+            Console.WriteLine("Уникальные числа списка: " + string.Join(", ", uniqNumbers.ToArray()));
+        }
+    }
+}
